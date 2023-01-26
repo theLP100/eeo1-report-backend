6905b86ee9215916c18db86f56c8b028abd20c00
LP <lp.passarelli.wilson@gmail.com>
2023-01-26 10:20:42 -0800

    created the database eeo1_db_development.  generated migrations and upgraded.  it s all connected now:

--- app/__init__.py
+++ app/__init__.py
@@ -12,11 +12,13 @@ load_dotenv
 def create_app(test_config=None):
     app = Flask(__name__)
     CORS(app)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     from app.models.Eeo1_data_line import Eeo1_data_line
     
     db.init_app(app)
+    migrate.init_app(app, db)
     
     from .routes.routes import query_bp
     app.register_blueprint(query_bp)
