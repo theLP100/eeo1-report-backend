7c3aa8f1f5eecb14e83aabb119ed30b14fe54368
LP <lp.passarelli.wilson@gmail.com>
2023-01-30 14:41:35 -0800

    made the get route filter to only include dates in 2021. next I'll generalize this following front end patterns.

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -16,11 +16,13 @@ def read_all_data():
     #this should return all the data we have.
     #add a query option for single queries.
     #THIS ROUTE NEEDS TESTING
-    data = Eeo1_data.query.all()
+    data = Eeo1_data.query.filter_by(year=2021).all()
     response = [data_line.to_dict() for data_line in data]
     # THE FOLLOWING DIDN'T WORK.
     # 
     # data = session.query(Eeo1_data_line.gender, func.sum(Eeo1_data_line.count_employees)).group_by(Eeo1_data_line.gender).all()
     # returns this query (it's not the right one): [SQL: SELECT eeo1_data_line.gender AS eeo1_data_line_gender, sum(eeo1_data_line.count_employees) AS sum_1 FROM eeo1_data_line GROUP BY eeo1_data_line.gender]
     return jsonify(response)
+
+#for now, I'm going to try to make a different route
 
