13e18635ec7ea89204f550eac5d3df7606c8379c
Margaret <y.margaret.lu+github@gmail.com>
2023-01-30 15:19:55 -0800

    add db import to fix query object

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from app.models.Eeo1_data import Eeo1_data
+from app import db
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
@@ -16,11 +17,16 @@ def read_all_data():
     #this should return all the data we have.
     #add a query option for single queries.
     #THIS ROUTE NEEDS TESTING
-    data = Eeo1_data.query.all()
-    response = [data_line.to_dict() for data_line in data]
+    # data = Eeo1_data.query.all()
+    # response = [data_line.to_dict() for data_line in data]
+    gender_totals = db.session.query(Eeo1_data.race, func.sum(Eeo1_data.count_employees)).group_by(Eeo1_data.race).all()
+    return_dict = {}
+    for gender, count_employees_total in gender_totals:
+        return_dict[gender] = count_employees_total
+    return jsonify(return_dict)
     # THE FOLLOWING DIDN'T WORK.
     # 
     # data = session.query(Eeo1_data_line.gender, func.sum(Eeo1_data_line.count_employees)).group_by(Eeo1_data_line.gender).all()
     # returns this query (it's not the right one): [SQL: SELECT eeo1_data_line.gender AS eeo1_data_line_gender, sum(eeo1_data_line.count_employees) AS sum_1 FROM eeo1_data_line GROUP BY eeo1_data_line.gender]
-    return jsonify(response)
+    
 
